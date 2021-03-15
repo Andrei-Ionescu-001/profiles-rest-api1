43c4949c3f3cf3854f98a8245e3444556c52eb69
andrei <andrei.ionescuu@gmail.con>
2021-03-15 08:33:11 +0200

    added login api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -8,6 +8,8 @@ router.register('profile', views.UserProfileViewSet, base_name='hello-viewset')
 
 urlpatterns=[
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
+
 
 ]
